2508e7d7597a51f359ca5d578439c08f38a94140
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-03 23:15:28 +0530

    Refactored: Ability to update the salary using Prepared Statement

--- test/java/com/bl/jdbcassignment/EmployeePayrollServiceTest.java
+++ test/java/com/bl/jdbcassignment/EmployeePayrollServiceTest.java
@@ -1,6 +1,7 @@
 package com.bl.jdbcassignment;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import java.sql.SQLException;
 import java.util.List;
@@ -15,5 +16,16 @@ public class EmployeePayrollServiceTest {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService(); 
 		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeeData(IOService.DB_IO);
 		assertEquals(4, employeePayrollData.size());
+	}
+	
+	@Test
+	public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDb() throws SQLException
+	{
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(); 
+		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeeData(IOService.DB_IO);
+		employeePayrollService.updateEmployeePayrollSalary("Bill", 200000.00);
+		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Bill");
+		assertEquals(4, employeePayrollData.size());
+		assertTrue(result);
 	}
 }
