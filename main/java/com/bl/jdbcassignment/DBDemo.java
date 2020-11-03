28ec0e18176ae23d6e752193c4e4b44609b233f2
Ishani Mhatre <mhatreishani1999@gmail.com>
2020-11-03 14:01:28 +0530

    Added: Ability to retrieve the Employee Payroll data

--- main/java/com/bl/jdbcassignment/DBDemo.java
+++ main/java/com/bl/jdbcassignment/DBDemo.java
@@ -6,5 +6,32 @@ import java.sql.DriverManager;
 import java.util.Enumeration;
 
 public class DBDemo {
-	
+	public static void main(String[] args) {
+		String jdbcurl =  "jdbc:mysql://localhost:3306/emp_payroll?useSSL=false";
+		String userName = "root";
+		String password = "root";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver Loaded Successfully");
+		} catch (ClassNotFoundException exception) {
+			throw new IllegalStateException("Cannot find driver in the classpath", exception);
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database: " + jdbcurl);
+			connection = DriverManager.getConnection(jdbcurl, userName, password);
+			System.out.println("Connection is successful: " + connection);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("  " + driverClass.getClass().getName());
+		}
+	}
 }
